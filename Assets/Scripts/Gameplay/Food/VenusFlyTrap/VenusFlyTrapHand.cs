ce345ad964769bdd8ad3d0631d1201f89239dea7
rivesuvacrispa <vova.cpeda@yandex.ru>
2025-09-20 00:27:31 +0300

    Feature: Venus fly trap now opens traps with a delay of 3 seconds to prevent instant kill when it spawns on top of you

--- Assets/Scripts/Gameplay/Food/VenusFlyTrap/VenusFlyTrapHand.cs
+++ Assets/Scripts/Gameplay/Food/VenusFlyTrap/VenusFlyTrapHand.cs
@@ -1,37 +1,68 @@
-﻿using Gameplay.Enemies;
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Gameplay.Enemies;
 using Player;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Gameplay.Food.VenusFlyTrap
 {
     public class VenusFlyTrapHand : MonoBehaviour
     {
         [SerializeField] private VenusFlyTrap flyTrap;
-        [SerializeField] private Sprite catchedSprite;
+        [SerializeField] private Sprite openedSprite;
+        [FormerlySerializedAs("catchedSprite")] 
+        [SerializeField] private Sprite closedSprite;
         [SerializeField] private SpriteRenderer spriteRenderer;
         
         private new Collider2D collider;
 
         private void Awake() => collider = GetComponent<Collider2D>();
 
+        private void Start()
+        {
+            Close();
+            OpenTrapTask(gameObject.GetCancellationTokenOnDestroy()).Forget();
+        }
+
+        private void Close()
+        {
+            spriteRenderer.sprite = closedSprite;
+            collider.enabled = false;
+        }
+
+        private void Open()
+        {
+            spriteRenderer.sprite = openedSprite;
+            collider.enabled = true;
+        }
+
+        private async UniTaskVoid OpenTrapTask(CancellationToken cancellationToken)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(3f), cancellationToken: cancellationToken);
+            Open();
+        }
+
         private void OnTriggerEnter2D(Collider2D col)
         {
-            bool catched = false;
+            bool caught = false;
             if (col.gameObject.TryGetComponent(out Enemy enemy))
             {
                 enemy.Die();
-                catched = true;
+                caught = true;
             }
             else if (col.gameObject.TryGetComponent(out PlayerManager player))
             {
                 player.Die(false);
-                catched = true;
+                caught = true;
             }
             
-            if(!catched) return;
-            spriteRenderer.sprite = catchedSprite;
-            collider.enabled = false;
-            flyTrap.Catch();            
+            if (caught)
+            {
+                Close();
+                flyTrap.Catch();
+            }          
         }
     }
 }
